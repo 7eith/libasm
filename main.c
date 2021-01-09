d6a887574d7e68c0fd6c3f59973fdd5592795f44
seith <isnkh@protonmail.com>
2021-01-09 14:13:54 +0100

    :sparkles: add: tests

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: amonteli <amonteli@student.42lyon.fr>      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/04 15:35:49 by amonteli          #+#    #+#             */
-/*   Updated: 2021/01/08 11:24:11 by amonteli         ###   ########lyon.fr   */
-/*                                                                            */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   main.c                                           .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: amonteli <amontelimart@gmail.com>          +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2021/01/04 15:35:49 by amonteli     #+#   ##    ##    #+#       */
+/*   Updated: 2021/01/09 14:11:39 by amonteli    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
 /* ************************************************************************** */
-
 #include <stdio.h>
 #include <string.h>
 #include "libasm.h"
@@ -20,5 +20,14 @@ int		main(void)
 	printf("%lu:%zu\n", strlen("salut"), ft_strlen("salut"));
 	printf("%lu:%zu\n", strlen(""), ft_strlen(""));
 
+	char dst[5];
+	char *src = "hola";
 
+	printf("origin  : [%s]\n", strcpy(dst, src));
+    printf("mine    : [%s]\n", ft_strcpy(dst, src));
+
+	printf("origin  : [%s]\n", strcpy(dst, ""));
+    printf("mine    : [%s]\n", ft_strcpy(dst, ""));
+
+	
 }
