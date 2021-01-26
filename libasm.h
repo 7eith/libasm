8e0067c4ffe79f592a05c3048272f6b659b36c2b
Alexis M <iamsnkh@pm.me>
2021-01-26 15:43:34 +0100

    :sparkles: add: provide tester

--- libasm.h
+++ libasm.h
@@ -2,6 +2,8 @@
 
 # include <string.h>
 # include <stdio.h>
+# include <errno.h>
+# include <stdlib.h>
 
 size_t			ft_strlen(const char *str);
 char            *ft_strcpy(char *dest, const char *src);
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 15:35:49 by amonteli          #+#    #+#             */
-/*   Updated: 2021/01/26 14:42:56 by user42           ###   ########lyon.fr   */
+/*   Updated: 2021/01/26 15:43:22 by user42           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ int		main(void)
 
 	char dst[5];
 	char *src = "hola";
-
 
 	printf("\t ft_strcmp\n\n");
 
@@ -74,25 +73,46 @@ int		main(void)
 
 	char *test_write = "salu";
 
-	// printf("origin : [%ld]\n", write(1, "test_write", 4));
-	// printf("mine   : [%ld]\n", ft_write(1, "test_write", 4));
+	printf("origin : [%ld]\n", write(1, "test_write", 4));
+	printf("mine   : [%ld]\n", ft_write(1, "test_write", 4));
 
-	// printf("\n");
+	printf("\n");
 
-	// printf("origin : [%ld]\n", write(1, "test_write", -1));
-	// printf("mine   : [%ld]\n", ft_write(1, "test_write", -1));
+	printf("origin : [%ld]\n", write(1, "test_write", -1));
+	printf("mine   : [%ld]\n", ft_write(1, "test_write", -1));
 
-	// printf("\n");
+	printf("\n");
 
-	// printf("origin : [%ld]\n", write(-1, "test_write", -1));
-	// printf("mine   : [%ld]\n", ft_write(-1, "test_write", -1));
+	printf("origin : [%ld]\n", write(-1, "test_write", -1));
+	printf("mine   : [%ld]\n", ft_write(-1, "test_write", -1));
 
-	int fd;
-	char *buf;
+    printf ("\n\n\t FT_READ ");
+    int fd;
+    char *buf, *buf2;
+    buf = malloc(sizeof(char*));
+    buf2 = malloc(sizeof(char*));
+    fd = open("tester",O_RDONLY);
+    printf("read = %zd errno = %s\n",read(fd,buf,10),strerror(errno));
+    printf("buf = %s\n",buf);
+    close(fd);
+    fd = open("tester",O_RDONLY);
+    printf("ft_read = %zd errno = %s\n",ft_read(fd,buf2,10),strerror(errno));
+    printf("buf = %s\n",buf2);
+    close(fd);
+    fd = open("tester",O_RDONLY);
+    printf("read = %zd errno = %s\n",read(fd,NULL,10),strerror(errno));
+    close(fd);
+    fd = open("tester",O_RDONLY);
+    printf("ft_read = %zd errno = %s\n",ft_read(fd,NULL,10),strerror(errno));
+    close(fd);
+    printf("read = %zd errno = %s\n",read(-1,NULL,1024),strerror(errno));
+    printf("ft_read = %zd errno = %s\n",ft_read(-1,NULL,1024),strerror(errno));
 
-	fd = open('./tests/read_me.txt', O_RDONLY);
-
-	read(fd, buf, 1);
-
-	printf("%s\n", buf);
+	char *c;
+    printf("strdup    = %s\n",c = strdup("MINISHELLLLLLL"));
+    printf("ft_strdup = %s\n",c = ft_strdup("MINISHELLLLLLL"));
+    printf("strdup    = %s\n",c = strdup("JE HAIS SAINT DENISSS"));
+    printf("ft_strdup = %s\n",c = ft_strdup("JE HAIS SAINT DENISSS"));
+    printf("strdup    = %s\n",c = strdup(""));
+    printf("ft_strdup = %s\n",c = ft_strdup(""));
 }
